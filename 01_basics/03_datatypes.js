6fb3e9a7cbee107dc854ae6d3eb8a7987fabc73f
Kaushiki Sahu <153459837+Kaushiki44@users.noreply.github.com>
2024-12-10 18:06:08 +0000

    datatypes

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,13 @@
+/* 
+number=> 2 power 53
+bigint (for bigger numbers)
+string=> ""
+boolean => true/false
+null => standalone value (empty)
+undefined (not assigned)
+symbol => unique
+object
+*/
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);//object
