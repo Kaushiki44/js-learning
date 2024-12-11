362619e92a41b7f6eec5b4a74a4283f2bd40ebd2
Kaushiki Sahu <153459837+Kaushiki44@users.noreply.github.com>
2024-12-11 10:40:27 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score=33
+console.log(typeof score); // number    
+console.log(typeof(score)); //number
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/* 
+
+"33" => 33
+"33abc" => NaN
+true => 1, false =>0
+
+*/
+
+let isLoggedIn=1
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+/* 
+1=>true 0=> false
+"" => false
+"kaush" => true
+
+*/
+
+let no= 33
+let stringNo = String(no);
+console.log(stringNo);
+ console.log(typeof stringNo);
