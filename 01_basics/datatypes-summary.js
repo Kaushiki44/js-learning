fecce0fe49030a665cca4f2c4ec164c348e25cc4
Kaushiki Sahu <153459837+Kaushiki44@users.noreply.github.com>
2024-12-15 11:38:51 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+//JavaScript is dynamically typed This means that you can assign any type of value to a variable without explicitly declaring the type
+
+//primitive
+//7types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score=100
+// const scoreValue=100.2
+
+// const isLoggedIn= false
+// const outsideTemp=null
+let userEmail;// this also means variable has undefined
+let userEmail2=undefined;
+
+// const id= Symbol('123');
+// const id2= Symbol('123');
+// console.log(id===id2);// -> false bcz symbol ka output doosra datatype hota h
+
+//const bigNum=232336753422145678925654783904234868365835351n;
+//console.log(typeof bigNum);// bigInt
+
+
+//REFERENCE ( non primitive)
+// arrays, objects, functions
+
+const heros=["perman", "doraemon", "krish"];
+let myObj={
+    name: "kaushiki",
+    age: "20",
+}
+
+const myFunction=function(){
+    console.log("hello world");
+    
+}
+console.log(typeof bigNum);// after commenting it becomes undefined
+
