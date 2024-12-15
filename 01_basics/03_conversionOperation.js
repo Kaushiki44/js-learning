9b05f61b73c7ec6196d6b94136edee90d934c130
Kaushiki Sahu <153459837+Kaushiki44@users.noreply.github.com>
2024-12-15 10:39:31 +0000

    conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score=33
-console.log(typeof score); // number    
-console.log(typeof(score)); //number
+//console.log(typeof score); // number    
+//console.log(typeof(score)); //number
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 /* 
 
@@ -16,8 +16,8 @@ true => 1, false =>0
 
 let isLoggedIn=1
 let booleanIsLoggedIn= Boolean(isLoggedIn);
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 /* 
 1=>true 0=> false
@@ -28,5 +28,40 @@ console.log(booleanIsLoggedIn);
 
 let no= 33
 let stringNo = String(no);
-console.log(stringNo);
- console.log(typeof stringNo);
+//console.log(stringNo);
+// console.log(typeof stringNo);
+
+
+ //OPERATIONS
+
+ let value=3;
+ let negValue=-value
+ //console.log(negValue);
+
+ //console.log(2+2);
+ //console.log(2-2);
+ //console.log(2*2);
+ //console.log(2**3);//power
+ //console.log(2%3);
+ //console.log(2/2);
+ 
+let s1="hello"
+let s2=" world"
+let s3=s1+s2
+//console.log(s3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+"2");
+//console.log("1"+2+2);//122
+//console.log(1+2+"2");//32
+
+console.log(+true);//1
+console.log(+"");//0
+
+let a,b,c;
+a=b=c=2+2; // -> possible but dont do it
+
+
+
+
