a4d5a250d217eeae6d0b9b257a8edb8210b46b98
Kaushiki Sahu <153459837+Kaushiki44@users.noreply.github.com>
2024-12-16 05:51:55 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,18 @@
+const name="kaushiki"//bts ye bhi object invoke krra
+const repoCount=50
+//console.log(name + repoCount + "Value");//old syntax
+
+//console.log(`my name is ${name} and my repo count is ${repoCount}`);//modern times syntax
+
+const str = new String('India')// invokes object
+
+console.log(str[0]);//I
+console.log(str.__proto__);//{}
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.charAt(3));
+console.log(str.indexOf('a'));
+
+
+
+
