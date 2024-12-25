508c76ef79dac482fa24ce1f7d7c2a1ec3f3bc20
Kaushiki Sahu <153459837+Kaushiki44@users.noreply.github.com>
2024-12-25 07:42:34 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,12 @@
+var d=40
+if(true){
+    let a=10
+    const b= 20
+    var c=30
+    d=90
+}
+//console.log(a); // a is not defined
+// console.log(b); // b is not defined
+console.log(c); // 30 
+//var apne scope ke bahar bhi kam krta h isilie ni use krte use
+console.log(d); //90 --> dikkat
