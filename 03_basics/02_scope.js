a8ffe55b4e8949fbdfbd5d40f0ff5978ea3c57a2
Kaushiki Sahu <153459837+Kaushiki44@users.noreply.github.com>
2024-12-25 08:13:43 +0000

    EXPRESSION

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -10,3 +10,20 @@ if(true){
 console.log(c); // 30 
 //var apne scope ke bahar bhi kam krta h isilie ni use krte use
 console.log(d); //90 --> dikkat
+
+
+//+++++++++++ INTERESTING
+
+console.log(addone(5)) // 6
+
+function addone(num){
+    return num+1
+}
+
+
+addTwo(5)// gives ERROR --expression cant be called before the actual expression
+
+//this is a function but it is also called EXPRESSION 
+const addTwo = function(num){
+    return num+2
+}
