3b42755da91fb87728531dec5e0b27da4df781d1
Kaushiki Sahu <153459837+Kaushiki44@users.noreply.github.com>
2024-12-26 12:08:15 +0000

    truthy

--- 04_control_flow.js/truthy.js
+++ 04_control_flow.js/truthy.js
@@ -18,3 +18,15 @@ if (Object.keys(emptyObj).length == 0) {
 // false ==0 ->true
 // false =='' ->true
 // 0 == '' ->true
+
+// Nullish Coalescing Operator (??): null undefined
+// The Nullish Coalescing Operator (??) in JavaScript is used to provide a default value when dealing with null or undefined
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+
+console.log(val1);
