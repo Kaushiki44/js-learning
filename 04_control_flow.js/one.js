f68a4c02dedf0c6a2b15c50d21425492b832eef0
Kaushiki Sahu <153459837+Kaushiki44@users.noreply.github.com>
2024-12-26 11:03:34 +0000

    !==

---
+++ 04_control_flow.js/one.js
@@ -0,0 +1,15 @@
+//   !==
+// Returns true if:
+// The values are not equal, or
+// The values are of different types.
+
+// Returns false if:
+// The values are equal and of the same type
+if(5!=='5')
+{
+    console.log("yes")//yes
+}
+
+if( 2==2) console.log("yes"), console.log("yes sur");
+//yes
+// yes sur
