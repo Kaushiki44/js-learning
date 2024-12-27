716aaff9f1b0e936c42be8706560c8a202b02fb6
Kaushiki Sahu <153459837+Kaushiki44@users.noreply.github.com>
2024-12-27 10:23:10 +0000

    for of

---
+++ 05_iterations.js/for_of.js
@@ -0,0 +1,53 @@
+//forof- maps, arrays, (not on objects)
+const numbers= [11, 12, 13, 14, 15]
+for (const num of numbers) {
+    // console.log(num);
+}
+// 11
+// 12
+// 13
+// 14
+// 15
+
+const greetings="HELLO WORLD!"
+for (const greet of greetings) {
+    // console.log(greet)
+}
+//H
+// E
+// L
+// L
+// O
+ 
+// W
+// O
+// R
+// L
+// D
+// !
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "America")
+map.set('Fr', "France")
+
+console.log(map)
+//Map(3) { 'IN' => 'India', 'USA' => 'America', 'Fr' => 'France' }
+
+
+for(const key of map){
+    console.log(key)
+}
+//[ 'IN', 'India' ]
+// [ 'USA', 'America' ]
+// [ 'Fr', 'France' ]
+// NOT PRINTING KEYS 
+//INSTEAD
+
+for (const [keya, valuea] of map) {
+    console.log(keya, ':-' , valuea);
+}
+//IN :- India
+// USA :- America
+// Fr :- France
+
