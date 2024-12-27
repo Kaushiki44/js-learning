16b9ee99ed4b05eb1e7ea443829efd142e0b5e22
Kaushiki Sahu <153459837+Kaushiki44@users.noreply.github.com>
2024-12-27 06:29:56 +0000

    truthy

--- 04_control_flow.js/truthy.js
+++ 04_control_flow.js/truthy.js
@@ -5,15 +5,12 @@
 // "0", 'false', " ", [], {}, function(){}
 
 const userEmail = []
-if (userEmail.length == 0) {
-    console.log("Array is empty");//Array is empty
-}
+// if (userEmail.length == 0) {
+//     console.log("Array is empty");//Array is empty
+// }
 
 const emptyObj = {}
 
-if (Object.keys(emptyObj).length == 0) {
-    console.log("Object is empty");//Object is empty
-}
 
 // false ==0 ->true
 // false =='' ->true
@@ -21,11 +18,11 @@ if (Object.keys(emptyObj).length == 0) {
 
 // Nullish Coalescing Operator (??): null undefined
 // The Nullish Coalescing Operator (??) in JavaScript is used to provide a default value when dealing with null or undefined
-let val1;
-// val1 = 5 ?? 10
-// val1 = null ?? 10
-// val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20
+let val1 
+val1 = 5 ?? 10 //5
+val1 = null ?? 10 //10
+// val1 = undefined ?? 15 //15
+// val1 = null ?? 10 ?? 20 //10
 
 
 
