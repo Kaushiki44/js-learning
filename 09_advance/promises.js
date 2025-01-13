a20b19c442c7a47d310c5e1e5276bdf4b63d9870
Kaushiki Sahu <153459837+Kaushiki44@users.noreply.github.com>
2025-01-13 11:19:22 +0000

    fetch

--- 09_advance/promises.js
+++ 09_advance/promises.js
@@ -146,3 +146,5 @@ fetch('https://jsonplaceholder.typicode.com/users')
     console.log(data);
 })
 .catch((error) => console.log(error))
+
+// A fetch() promise only rejects when a network error is encountered (which is usually when there's a permissions issue or similar). A fetch() promise does not reject on HTTP errors (404, etc.). Instead, a then() handler must check the Response.ok and/or Response.status properties.
