18e8a6f0428ea10d7961d19b79297a712b1c6a6a
Kaushiki Sahu <153459837+Kaushiki44@users.noreply.github.com>
2025-01-13 19:53:13 +0000

    constructor func

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -1,3 +1,5 @@
+//Object
+// Object Literal is a way to create an object using a simple syntax of key-value pairs enclosed within curly braces {}
 const user ={
     username: "kaushiki",
     loginCount: 10,
@@ -7,7 +9,7 @@ const user ={
         console.log("Got user details from database")
         console.log(`username is ${this.username}`)
         //necessary to use THIS here to refer to current context bcs in js engine context kho sa jata h
-        console.log(this);
+        //console.log(this);
         // {
         //     username: 'kaushiki',
         //     loginCount: 10,
@@ -19,9 +21,35 @@ const user ={
    
    
 }
-console.log(user.username);//kaushiki
-console.log(user.getUserDetails()); //Got user details from database
+// console.log(user.username);//kaushiki
+// console.log(user.getUserDetails()); //Got user details from database
 // username is kaushiki
 
-console.log(this) //{}
+// console.log(this) //{}
 //but in the browser it gives windows and a list of other things
+
+
+//CONSTRUCTOR FUNCTION
+// Before class syntax was introduced in ES6, constructor functions were used to create objects with reusable properties and methods.
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount,
+    this.isLoggedIn= isLoggedIn;
+
+    return this// implicitly bhi hota h, kro na kro same baat
+}
+
+//defining a structure and creating different users
+const userOne = new User("kaushiki", 12, true);
+const userTwo = new User("rashi", 1, false);
+console.log(userOne.constructor)//[Function: User]
+console.log(userOne);
+console.log(userTwo)
+// User { username: 'kaushiki', loginCount: 12, isLoggedIn: true }
+// User { username: 'rashi', loginCount: 1, isLoggedIn: false }
+
+// 1. new keyword creates an empty obj callled instance
+// 2. cons func is called due to new 
+// 3. this keyword wle sab usme injecr hote 
+// 4. fir sab hme milte
